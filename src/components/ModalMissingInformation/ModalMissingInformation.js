b115b266b554180118cf53d28a9271558a82b6b5
IDriuk <veytarn@gmail.com>
2021-05-12 13:39:31 +0300

    disable missing information modal for stripe

--- src/components/ModalMissingInformation/ModalMissingInformation.js
+++ src/components/ModalMissingInformation/ModalMissingInformation.js
@@ -85,7 +85,8 @@ class ModalMissingInformation extends Component {
       if (emailVerificationNeeded) {
         this.setState({ showMissingInformationReminder: EMAIL_VERIFICATION });
       } else if (stripeAccountNeeded) {
-        this.setState({ showMissingInformationReminder: STRIPE_ACCOUNT });
+        // StripeWorkaround ================
+        // this.setState({ showMissingInformationReminder: STRIPE_ACCOUNT });
       }
     }
   }
